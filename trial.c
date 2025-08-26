a4d6c0458e63a3490831bf1488aeb3588a6c366b
Druva <ddruva445@gmail.com>
2025-08-26 13:57:19 +0530

    Added more code

--- trial.c
+++ trial.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
+
+void print(int a, int b){
+  printf("%f", a/b);
+}
+
 int
 main (int argc, char **argv)
 {
   int i;
 
-  for (i = 0; i <= argc; i++){
+  for (i = 1; i < argc; i++){
     // printf("1. before %dth printf", i);
     printf ("%s ", argv[i]);
     // printf ("%s \n", argv[i]);
     // printf("2. i=<%d>\n", i);
-    // printf("%f", 1/0);
+    // print(1, 0);
   }
   printf ("\n");
-  printf("3. Finished\n");
+  // printf("3. Finished\n");
 
   return 0;
 }
